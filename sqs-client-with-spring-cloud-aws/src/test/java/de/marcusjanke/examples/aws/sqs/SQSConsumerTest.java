8612270597cfbe64fc552d2cff56b4257672c390
marcus-j <18111840+marcus-j@users.noreply.github.com>
2019-03-28 08:39:36 +0100

    adds link to suitable testing template

--- sqs-client-with-spring-cloud-aws/src/test/java/de/marcusjanke/examples/aws/sqs/SQSConsumerTest.java
+++ sqs-client-with-spring-cloud-aws/src/test/java/de/marcusjanke/examples/aws/sqs/SQSConsumerTest.java
@@ -1,5 +1,9 @@
 package de.marcusjanke.examples.aws.sqs;
 
+import org.elasticmq.rest.sqs.SQSRestServer;
+import org.elasticmq.rest.sqs.SQSRestServerBuilder;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,9 +13,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = SQSConsumer.class)
 public class SQSConsumerTest {
 
+    private static SQSRestServer sqsRestServer;
+
+    @BeforeClass
+    public static void setUp() {
+        sqsRestServer = SQSRestServerBuilder.start();
+        sqsRestServer.waitUntilStarted();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        sqsRestServer.stopAndWait();
+    }
+
     @Test
     public void shouldConsumeScheduledMessages() {
         //TODO: Figure out a good way to integration test this without AWS or local cloud instance
+        // This might help: http://codedevstuff.blogspot.com/2017/12/setup-local-aws-sqs-service-with.html
     }
 
 }
